d3a1f8ff948cec9ca12d2cfe9eed47c92bc54cdf
Archita Desai <architad8@gmail.com>
2019-07-28 22:10:47 +0530

    Implement Checkout and Search book feature

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 All-in-one place to Browse, Search & Buy books conveniently.
 
-
 ## Installation Instructions
 - After unzipping (or cloning) the repository, go inside the directory containing the project.
+- Note: You can clone the repository from following URL:
+    - `git clone https://github.com/ArchitaDesai/books-app-react`
     - ` cd books-app-react `
 - Install all the dependencies:
     - `npm install`
@@ -16,7 +17,9 @@ All-in-one place to Browse, Search & Buy books conveniently.
 - Fetched the books data from API
     - Used a CORS-anywhere API to overcome cross-site origin error when fetching data from the API
 - The books are displayed as a sorted list (sorted by books' ID)
-- Ratings of books are displayed in the format
+- Ratings of books are displayed in the star format
+- Search books by Title and Author name
+- Checkout book to proceed to the Cart
 
 
 ## Tech Stack
@@ -28,11 +31,35 @@ All-in-one place to Browse, Search & Buy books conveniently.
     - To overcome cross-site origin error when fetching data from API
 - StarRatings 
     - Node Module, to display ratings in star format
-
+- Fontawesome
+    - For icons
 
 
 ## Screenshots
 
-![Mobile List View](readme_images/mobile-view.jpg)
-![Desktop View Loading](readme_images/loading.png)
-![Desktop List View](readme_images/desktop-list-view.png)
+###### List of Books (Desktop)
+![List Books Desktop View](readme_images/list-books-desktop.png)
+
+###### Star Rating Tooltip (Desktop)
+![Star Rating Tooltip on Hover Desktop](readme_images/star-rating-tooltip.png)
+
+<br />
+
+###### Search Books (Desktop)
+
+<div style="margin: 0 auto">
+    <!-- <img src="readme_images/search-books.png" alt="Search Books"/> -->
+    <img src="readme_images/search-books-by-title.png" alt="Search Book Displays by Title"/>
+    <!-- <img src="readme_images/search-books-by-authors.png" alt="Search Book Displays by Authors"/> -->
+</div>
+
+###### Checkout Book (Desktop)
+![Checkout Book Desktop View](readme_images/checkout-book-desktop.png)
+
+<br/><br/><br/>
+
+###### Respoonsive React App - Works well in Tablet as well as in Mobile
+<div style="margin: 0 auto">
+    <img src="readme_images/list-books-mobile.png" alt="List Books Mobile View"/>
+    <img src="readme_images/checkout-book-mobile.png" alt="Checkout Book Mobile"/>
+</div>
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import BooksList from './components/books/BooksList';
+import Checkout from './components/books/Checkout';
 
 class App extends Component {
 
@@ -8,9 +9,54 @@ class App extends Component {
         super();
         this.state = {
             books: [],
-            loading: false,
-        }
-    }
+			loading: false,
+			checkoutBook: false,
+			checkoutBookID: -1,
+			showSearchedBooks: false,
+			searchedBooks: []
+		}
+		this.handleBookCheckout = this.handleBookCheckout.bind(this);
+		this.searchBooks = this.searchBooks.bind(this);
+	}
+	
+	handleBookCheckout(event, book_id) {
+        this.setState({
+            checkoutBook: true,
+            checkoutBookID: book_id
+        });
+	}
+	
+	searchBooks(event) {
+		if(event.key === 'Enter') {
+
+			let search_term = event.target.value.toLowerCase();
+			const books = this.state.books;
+			let resulted_books = [];
+			// Find books by title & authors having the search term
+			books.find(function(element){
+				let current_book = element;
+				if(typeof current_book.title === 'string' || current_book.title instanceof String) {
+					let current_book_title = current_book.title.toLowerCase();
+					let current_book_author = current_book.authors.toLowerCase();
+
+					if(current_book_title.includes(search_term)) {
+						resulted_books.push(element);
+					}
+					else if(current_book_author.includes(search_term)) {
+						resulted_books.push(element);
+					}
+				}
+			})
+
+			// If books exist for searched keyword
+			if(resulted_books.length !== 0) {
+				this.setState({
+					showSearchedBooks: true,
+					searchedBooks: resulted_books
+				});
+			}
+		}
+	}
 
     componentDidMount() {
         this.setState({
@@ -45,10 +91,32 @@ class App extends Component {
         }
 
         else if(books !== "Loading" && Object.keys(books).length !== 0) {
-			console.log("Books are : ", books.slice(0,10))
-			console.log("Total books: ", books.length)
-			let someBooks = books.slice(0,20);
-			// let self = this;
+			 // When a book will be checked out
+			if(this.state.checkoutBook === true) {
+				let book_id = this.state.checkoutBookID;
+				// returns the index of element in the list with our desired book ID
+				let book_index_in_list = books.findIndex((elem) => elem.bookID === book_id)
+
+                return(
+                    <Checkout 
+						title={ books[book_index_in_list].title }
+						authors= {books[book_index_in_list].authors }
+						price={ books[book_index_in_list].price }
+                    />
+                )
+			}
+			
+			let someBooks;
+			if(this.state.showSearchedBooks === true) {
+				someBooks = this.state.searchedBooks;
+			}
+			// Show first 20 books when app loads
+			else {
+				someBooks = books.slice(0,20);
+			}
+			// console.log("Total books in DB: ", books.length)
+
+			let self = this;
 			const booksComponents = someBooks.map(function(book, index){ 
 				return(
 					<BooksList key={ index }
@@ -60,6 +128,7 @@ class App extends Component {
 							ratingCount={ book.ratings_count }
 							langCode={ book.language_code }
 							isbn={ book.isbn }
+							checkoutBook={self.handleBookCheckout}
 					/>
 				)            
 			});
@@ -67,7 +136,9 @@ class App extends Component {
 				<div>
 					<input type="search" 
 						className="form-control"
-						placeholder="Search by Book or Author's Name..." />
+						placeholder="Search by Book or Author's Name..." 
+						onKeyDown={ this.searchBooks }
+					/>
 					
 					<div className="table-responsive">
 						<table className="table table-dark table-hover">
